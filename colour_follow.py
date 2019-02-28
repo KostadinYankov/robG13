da294e284f30927dad1069b4c6ddf26471922b0a
KostadinYankov <32684892+KostadinYankov@users.noreply.github.com>
2019-02-28 19:30:32 +0000

    Add files via upload

--- colour_follow.py
+++ colour_follow.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-
+# import 
 import numpy
-
 import cv2
 import cv_bridge
 import rospy
-
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 
@@ -19,49 +17,84 @@ class Follower:
                                           self.depth_image_callback)
         self.cmd_vel_pub = rospy.Publisher('/mobile_base/commands/velocity', Twist,
                                            queue_size=1)
-        self.depth_image_CV2 = None
+        self.depth_image_CV2 = ""
+            #print(depth_image_CV2)
+        
         self.twist = Twist()
-
+        tw = Twist()
+        tw.angular.z = twist
 
     def depth_image_callback(self, data):
-        self.depth_image_CV2 = self.bridge.imgmsg_to_cv2(data, "32FC1")
+        self.depth_image_CV2 = self.bridge.imgmsg_to_cv2(data)
 
 
-    def image_callback(self, msg):
-        cv2.namedWindow("window", 1)
-        cv2.namedWindow("colourSeg", 2)
-        image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        lower_red = numpy.array([0, 120, 100])
-        upper_red = numpy.array([20, 360, 360])
-        mask = cv2.inRange(hsv, lower_red, upper_red)
-        colourSegImage = cv2.bitwise_and(image, image, mask=mask)
-        h, w, d = image.shape
-        search_top = h / 4
-        search_bot = 3*h/4 + 20
-        mask[0:search_top, 0:w] = 0
-        mask[search_bot:h, 0:w] = 0
-        M = cv2.moments(mask)
-        if M['m00'] > 0:
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            # cv2.circle(image, (cx, cy), 20, (255, 0, 255), -1)
-            cv2.circle(image, (cx, cy), 20, (255, 0, 255), -1)
-            err = cx - w/2
-            self.twist.linear.x = 0.2
-            self.twist.angular.z = -float(err) / 100
+    def image_callback(self, message):
+        # create windows to display 
+        cv2.namedWindow("RobotView", 1)
+        cv2.namedWindow("SegmentedView", 2)
+        # send message of an image to openCv as image 
+        imageRobot = self.bridge.imgmsg_to_cv2(message, desired_encoding='bgr8')
+        # convert BGR to HSV
+        # convert as RGB color into Degrees and *2/3
+        hsv = cv2.cvtColor(imageRobot, cv2.COLOR_BGR2HSV)
+        # RED mask
+        lowerRed = numpy.array([0, 120, 100])
+        upperRed = numpy.array([6, 255, 255])
+        maskRed = cv2.inRange(hsv, lowerRed, upperRed)
+        maskRed = cv2.medianBlur(maskRed,7)
+        # GREEN mask
+        lowerGreen = numpy.array([110, 120, 100])
+        upperGreen = numpy.array([130, 255, 255])
+        maskGreen = cv2.inRange(hsv, lowerGreen, upperGreen)
+        maskGreen = cv2.medianBlur(maskGreen,7)
+        # BLUE mask
+        lowerBlue = numpy.array([60, 120, 100])
+        upperBlue = numpy.array([70, 255, 255])
+        maskBlue = cv2.inRange(hsv, lowerBlue, upperBlue)
+        maskBlue = cv2.medianBlur(maskBlue,7)
+        # YELLOW mask
+        lowerYellow = numpy.array([30, 120, 100])
+        upperYellow = numpy.array([40, 255, 255])
+        maskYellow = cv2.inRange(hsv, lowerYellow, upperYellow)
+        maskYellow = cv2.medianBlur(maskYellow,7)
+        
+        allMasks= maskRed+maskGreen+maskBlue+maskYellow
+        # use mean filter for noise such as salt and pepper (issue faced, and sorted)
+         
+        # filter again with medain filter
+        colourAvgFilt = cv2.blur(allMasks,(5,5)) 
+        
+        height, width, d = imageRobot.shape
+        
+        searchTop = height / 4
+        searchBot = ((3*height)/4) + 20
+        colourAvgFilt[0:searchTop, 0:width] = 0
+        colourAvgFilt[searchBot:height, 0:width] = 0
+        move = cv2.moments(colourAvgFilt)
+        if move['m00'] > 0:
+            cx = int(move['m10']/move['m00'])
+            cy = int(move['m01']/move['m00'])
+            # cv2.circle(imageRobot, (cx, cy), 20, (255, 0, 255), -1)
+            cv2.circle(imageRobot, (cx, cy), 20, (255, 0, 255), -1)
+            error = cx - width/2
+            self.twist.linear.x = 1
+            self.twist.angular.z = -float(error) / 100
 
-            cDepth = self.depth_image_CV2[cy, cx]
-            print(cDepth)
+            distanceToObject = self.depth_image_CV2[cy, cx]
+            print(distanceToObject)
 
-            if cDepth < 1.0:
+            if distanceToObject < 1.0:
                 self.twist.linear.x = 0.0
                 self.twist.angular.z = 0.0
 
 
-            self.cmd_vel_pub.publish(self.twist)
-        cv2.imshow("window", image)
-        cv2.imshow("colourSeg", colourSegImage)
+            #self.cmd_vel_pub.publish(self.twist)
+        cv2.imshow("RobotView", imageRobot)
+        cv2.imshow("SegmentedView", colourAvgFilt)
+        cv2.imshow("MaskRed", maskRed)
+        cv2.imshow("MaskGreen", maskGreen)
+        cv2.imshow("MaskBlue", maskBlue)
+        cv2.imshow("MaskYellow", maskYellow)
         cv2.waitKey(3)
 
 
