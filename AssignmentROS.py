1e518f7ad3c6bedad87bbf099c70a9765ea94d2e
KostadinYankov <32684892+KostadinYankov@users.noreply.github.com>
2019-03-05 12:11:55 +0000

    Add files via upload

--- AssignmentROS.py
+++ AssignmentROS.py
@@ -7,6 +7,13 @@ import rospy
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 
+#######################################################################################
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from actionlib_msgs.msg import GoalStatus
+
+
+
 
 class Follower:
     def __init__(self):
@@ -25,7 +32,48 @@ class Follower:
         
         # assign new variable to self from Twist type
         self.twist = Twist()
-        
+####################################################################################################
+    
+        rospy.init_node('follower', anonymous=False)
+
+	# what to do if shut down (e.g. ctrl + C or failure)
+	rospy.on_shutdown(self.shutdown)
+
+	
+	#tell the action client that we want to spin a thread by default
+	self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+	rospy.loginfo("wait for the action server to come up")
+	#allow up to 5 seconds for the action server to come up
+	self.move_base.wait_for_server(rospy.Duration(5))
+
+	#we'll send a goal to the robot to move 3 meters forward
+	goal = MoveBaseGoal()
+	goal.target_pose.header.frame_id = 'base_link'
+	goal.target_pose.header.stamp = rospy.Time.now()
+	goal.target_pose.pose.position.x = 3.0 #3 meters
+	goal.target_pose.pose.orientation.w = 1.0 #go forward
+
+	#start moving
+        self.move_base.send_goal(goal)
+
+	#allow TurtleBot up to 60 seconds to complete task
+	success = self.move_base.wait_for_result(rospy.Duration(60)) 
+
+
+	if not success:
+                self.move_base.cancel_goal()
+                rospy.loginfo("The base failed to move forward 3 meters for some reason")
+    	else:
+		# We made it!
+		state = self.move_base.get_state()
+		if state == GoalStatus.SUCCEEDED:
+		    rospy.loginfo("Hooray, the base moved 3 meters forward")
+
+
+
+    def shutdown(self):
+        rospy.loginfo("Stop")
+#################################################################################################
         # placeholder values set for HSV masks
         self.maskRed = None
         self.maskGreen = None
